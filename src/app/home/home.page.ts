895b67e29754cb0d688b85af31211cb88e25004b
lonelystar16 <134518378+lonelystar16@users.noreply.github.com>
2024-08-27 23:03:46 -0400

    JSON obj called user with validation statements, NavExtras to send info at pag named 'registro'

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // Objeto JSON que se enviará a la página de detalles
+  user = {
+    username: '',
+    password: ''
+  };
+  // Mensaje 
+  mensaje = '';
 
-  constructor() {}
+// Constructor
+  constructor(private router: Router) {}
 
+  // Método que se ejecuta al hacer clic en el botón
+  validar() {
+    if (this.user.username.length != 0)
+    {
+      if (this.user.password.length != 0)
+      {
+      // Funciona
+      this.mensaje = 'Usuario y contraseña correctos';
+      // Crear objeto de navegación
+      let navigationExtras: NavigationExtras = {
+        state: {
+          user: this.user.username,
+          pass: this.user.password
+        },
+      };
+      // 
+      this.router.navigate(['/registro'], navigationExtras);
+    } else {
+      // No funciona
+      this.mensaje = 'Contraseña incorrecta';
+    }
+  } else {
+    // No funciona
+    this.mensaje = 'Usuario incorrecto';
+  }
 }
+}
+
