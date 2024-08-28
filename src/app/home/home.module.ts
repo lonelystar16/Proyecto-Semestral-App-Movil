43c1c9dc974630cd135dbfd7ab50151671037a86
lonelystar16 <134518378+lonelystar16@users.noreply.github.com>
2024-08-28 19:58:51 -0400

    Added new module called MatProgressSpinnerModule from Angular Components

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    MatProgressSpinnerModule
   ],
   declarations: [HomePage]
 })
