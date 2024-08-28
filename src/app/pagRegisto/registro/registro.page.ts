f7ad80869a291dd6d1c3d049fa432f49e7cb1fdf
lonelystar16 <134518378+lonelystar16@users.noreply.github.com>
2024-08-27 23:21:20 -0400

    The code changes add functionality to the registration page by retrieving the username from the previous page and assigning it to the `username` variable. This allows the registration page to display the username during initialization.

--- src/app/pagRegisto/registro/registro.page.ts
+++ src/app/pagRegisto/registro/registro.page.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router} from '@angular/router';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
+  username = '';
 
-  constructor() { }
-
+  // 
+  constructor(private router: Router) {
+    // 
+    const navigation = this.router.getCurrentNavigation();
+    const state = navigation?.extras.state as {
+      user: '';
+      pass: '';
+    };
+    this.username = state.user;
+  }
   ngOnInit() {
   }
 
