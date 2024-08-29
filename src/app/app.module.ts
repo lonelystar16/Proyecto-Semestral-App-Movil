a4ec826f253c1e6c70cbfddb465f92e840ab4f6d
lonelystar16 <134518378+lonelystar16@users.noreply.github.com>
2024-08-28 20:03:55 -0400

    Add MatProgressSpinnerModule to app module and implement spinner functionality in home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
@@ -10,7 +10,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, MatProgressSpinnerModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
-
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -15,35 +15,64 @@ export class HomePage {
   };
   // Mensaje 
   mensaje = '';
+  spinner = false;
 
-// Constructor
-  constructor(private router: Router) {}
+
+  // Constructor
+  constructor(private router: Router, private animationController: AnimationController) { }
+
+  // Generar animación
+  generarAnimacion() {
+    const elemento = document.querySelector('#GwenIcon') as HTMLElement;
+    // Crear animación
+    const animacion = this.animationController.create()
+      .addElement(elemento)
+      .duration(3000)
+      .iterations(Infinity)
+      .fromTo('scale', '1', '0.5');
+
+    // Reproducir animación
+    animacion.play();
+  }
+  ngAfterContentInit() {
+    this.generarAnimacion();
+  }
+
+
 
   // Método que se ejecuta al hacer clic en el botón
+  cambiarSpinner() {
+    this.spinner = !this.spinner;
+  }
+
+
   validar() {
-    if (this.user.username.length != 0)
-    {
-      if (this.user.password.length != 0)
-      {
-      // Funciona
-      this.mensaje = 'Usuario y contraseña correctos';
-      // Crear objeto de navegación
-      let navigationExtras: NavigationExtras = {
-        state: {
-          user: this.user.username,
-          pass: this.user.password
-        },
-      };
-      // 
-      this.router.navigate(['/registro'], navigationExtras);
+    if (this.user.username.length != 0) {
+      if (this.user.password.length != 0) {
+        // Funciona
+        this.mensaje = 'Usuario y contraseña correctos';
+        // Crear objeto de navegación
+        let navigationExtras: NavigationExtras = {
+          state: {
+            user: this.user.username,
+            pass: this.user.password
+          },
+        };
+        this.cambiarSpinner()
+        setTimeout(() => {
+          this.router.navigate(['/registro'], navigationExtras);
+          this.cambiarSpinner();
+          this.mensaje = '';
+        }, 3000);
+        // 
+      } else {
+        // No funciona
+        this.mensaje = 'Contraseña incorrecta';
+      }
     } else {
       // No funciona
-      this.mensaje = 'Contraseña incorrecta';
+      this.mensaje = 'Usuario incorrecto';
     }
-  } else {
-    // No funciona
-    this.mensaje = 'Usuario incorrecto';
   }
-}
 }
 
