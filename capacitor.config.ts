c09b5deba8d459a321b15411b0ffd73afce66419
trollynnn <ma.quirogau@duocuc.cl>
2024-08-29 19:23:28 -0400

    angular y capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'appmovil',
+  appId: 'com.tellevo.app',
+  appName: 'tellevo-app',
   webDir: 'www'
 };
 
