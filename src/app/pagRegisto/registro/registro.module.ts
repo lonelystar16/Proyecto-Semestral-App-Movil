857faa6e56fe0f73d6c2b4e1aca4ba2b07f7e607
lonelystar16 <134518378+lonelystar16@users.noreply.github.com>
2024-08-29 13:27:51 -0400

    chore: Add Angular Material modules to registration page

--- src/app/pagRegisto/registro/registro.module.ts
+++ src/app/pagRegisto/registro/registro.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { RegistroPageRoutingModule } from './registro-routing.module';
-
 import { RegistroPage } from './registro.page';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RegistroPageRoutingModule
+    RegistroPageRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+
   ],
   declarations: [RegistroPage]
 })
-export class RegistroPageModule {}
+export class RegistroPageModule { }
