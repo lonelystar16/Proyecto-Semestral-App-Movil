ec6c2a0dd9101f1df685495071964013ee043be1
lonelystar16 <134518378+lonelystar16@users.noreply.github.com>
2024-09-02 00:55:26 -0400

    chore: Add Angular Material progress spinner and checkbox to login page

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -7,13 +7,18 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import { MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    MatProgressSpinnerModule,
+    MatCheckboxModule
   ],
   declarations: [LoginPage]
 })
