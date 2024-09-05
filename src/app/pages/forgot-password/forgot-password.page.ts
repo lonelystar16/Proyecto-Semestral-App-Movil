410c0ca8eeee99233dc1e858e86487decae96915
lonelystar16 <134518378+lonelystar16@users.noreply.github.com>
2024-09-05 12:00:07 -0400

    feat: Agregar validación de contraseñas en página de recuperación de contraseña

--- src/app/pages/forgot-password/forgot-password.page.ts
+++ src/app/pages/forgot-password/forgot-password.page.ts
@@ -1,15 +1,82 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.page.html',
   styleUrls: ['./forgot-password.page.scss'],
 })
 export class ForgotPasswordPage implements OnInit {
-
-  constructor() { }
+  // Objeto que almacena los datos del formulario de recuperación de contraseña
+  reset = {
+    first_password: '',
+    second_password: ''
+  };
 
   ngOnInit() {
   }
+
+
+  constructor(private AlertController: AlertController) { }
+
+  async presentAlert_PasswordRecovery() {
+    const alert = await this.AlertController.create({
+      cssClass: 'alerta-password-recovery',
+      header: 'Error',
+      message: 'Las contraseñas no coinciden.',
+      buttons: ['Aceptar'],
+      backdropDismiss: false
+    });
+    await alert.present();
+  }
+
+  async presentAlert_PasswordRecoveryEmpty() {
+    const alert = await this.AlertController.create({
+      cssClass: 'alerta-password-recovery',
+      header: 'Error',
+      message: 'Ambos campos están vacíos.',
+      buttons: ['Aceptar'],
+      backdropDismiss: false
+    });
+    await alert.present();
+  }
+
+  // Función para mostrar que las contraseñas se han cambiado correctamente
+  async validatePasswordRecovery() {
+    const alert = await this.AlertController.create({
+      cssClass: 'alerta-password-recovery',
+      header: 'Éxito',
+      message: 'Contraseñas cambiadas correctamente.',
+      buttons: ['Aceptar'],
+      backdropDismiss: false
+    });
+    await alert.present();
+  }
+
+
+
+  // Validar que las contraseñas sean iguales
+  // Validar de que si ambos campos están vacíos,se muestre el mensaje de error
+  validatePassword() {
+    if (this.reset.first_password === this.reset.second_password) {
+      if (this.reset.first_password === '' && this.reset.second_password === '') {
+        this.presentAlert_PasswordRecoveryEmpty();
+      } else {
+        this.validatePasswordRecovery();
+      }     
+    } else {
+      this.presentAlert_PasswordRecovery();
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
 
 }
