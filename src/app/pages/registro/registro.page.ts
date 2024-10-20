ae12203ac7222f64b668e521098513523596512b
Fernando Muñoz <134518378+lonelystar16@users.noreply.github.com>
2024-10-20 15:39:19 -0300

    Refactorizar página de registro para mejorar la apariencia y funcionalidad

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,8 +1,8 @@
+import { RegistroPageRoutingModule } from './registro-routing.module';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
 import { AuthenticatorService } from 'src/app/Servicios/authenticator.service';
-
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
@@ -21,35 +21,34 @@ export class RegistroPage implements OnInit {
   constructor(
     private router: Router,
     private auth: AuthenticatorService,
-    private toastController: ToastController
+    private toastController: ToastController,
   ) { }
 
   ngOnInit() {
   }
 
   async registrar() {
-    console.log(this.alumno);
     this.auth
-    .registrar(this.alumno)
-    .then((res) => {
-      this.router.navigate(['/login']);
-      return this.toastController.create({
-        message: 'Usuario registrado con éxito',
-        duration: 2000,
-        position: 'bottom',
-        color: 'success'
-      });
-    })
-    .then((toast) => toast.present())
-    .catch((error) => {
-      return this.toastController.create({
-        message: 'Error al registrar usuario',
-        duration: 2000,
-        position: 'bottom',
-        color: 'danger'
-      })
-      .then((toast) => toast.present())
-    })
+      .registrar(this.alumno)
+        .then((res) => {
+          this.router.navigate(['/login']);
+          return this.toastController.create({
+            message: 'Usuario registrado con éxito',
+            duration: 2000,
+            position: 'bottom',
+            color: 'success'
+          });
+        })
+        .then((toast) => toast.present())
+        .catch((error) => {
+          return this.toastController.create({
+            message: 'Error al registrar usuario',
+            duration: 2000,
+            position: 'bottom',
+            color: 'danger'
+          })
+          .then((toast) => toast.present());
+        })
   }
 
 
